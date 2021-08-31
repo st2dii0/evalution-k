0d3d6e004fabf09dc3b9a84ab4a200e2996abf68
st2diio <st2diio@protonmail.ch>
2021-08-31 10:13:00 +0200

    Adding routes and context to entrypoint App

--- evaluation_k/src/App.tsx
+++ evaluation_k/src/App.tsx
@@ -1,57 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import { Route } from "react-router-dom"
+import { UserProvider, UserState } from "./core/user"
 
-function App() {
+import { Home } from "./pages/Home"
+import { Chapters } from "./pages/Chapters"
+import { Evaluation } from "./pages/Evaluation"
+import { Questions } from "./pages/Questions"
+import { CreateChapter } from "./pages/CreateChapter";
+
+import logo from './logo.svg'
+
+import './App.css'
+
+const user = localStorage.getItem("user");
+const token = localStorage.getItem("token");
+
+const userInitialState: UserState = {
+  user: user ? JSON.parse(user) : null,
+  token: token
+};
+
+const reducer = (prevState: UserState, action: UserAction): UserState => {
+  switch (action.type) {
+    case "setUser":
+      if (!action.payload) {
+        localStorage.removeItem("user");
+        return { ...prevState, user: null };
+      }
+
+      const user = { ...prevState.user, ...action.payload };
+      localStorage.setItem("user", JSON.stringify(user));
+      return { ...prevState, user };
+
+    case "setToken":
+      if (!action.payload) {
+        localStorage.removeItem("token");
+        return { ...prevState, token: null };
+      }
+      const token = action.payload;
+      localStorage.setItem("token", token);
+      return { ...prevState, token };
+
+    default:
+      return prevState;
+  }
+};
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <UserProvider initialState={userInitialState} reducer={reducer}>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/chapitres" component={Chapters} />
+      <Route exact path="/chapitres/nouveau" component={CreateChapter} />
+      <Route exact path="/evaluation" component={Evaluation} />
+      <Route exact path="/question" component={Questions} />
+    </UserProvider>
   );
 }
 
