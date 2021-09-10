e1a0bfabf41996ba64c3e063b7cb81cf4ddbac33
st2diio <st2diio@protonmail.ch>
2021-09-10 19:08:40 +0200

    updating chapter model

--- evaluation_k/src/models/api/Chapters.ts
+++ evaluation_k/src/models/api/Chapters.ts
@@ -9,13 +9,24 @@ export interface Chapter {
         updated_at: Date
         questions?: [
                 {
+                     id: number
+                     chapter_id: number
+                     text: string
+                     difficulty: number
+                     created_at: Date
+                     updated_at: Date
+                }
+        ],
+        chapters?: [
+                {
                         id: number
+                        field_id: number
+                        level_id: number
                         chapter_id: number
-                        text: string
-                        difficulty: number
+                        name: string
+                        number?: number
                         created_at: Date
                         updated_at: Date
                 }
-        ],
-        chapters: []
+        ]
 }
