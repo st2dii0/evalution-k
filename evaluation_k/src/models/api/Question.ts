67f6dac19e93241d0e2dfad58bca635bc45b6ff2
st2diio <st2diio@protonmail.ch>
2021-09-28 00:07:09 +0200

    updatting question model

--- evaluation_k/src/models/api/Question.ts
+++ evaluation_k/src/models/api/Question.ts
@@ -1,11 +1,11 @@
+export interface Answers_Attributes {
+    correct: boolean
+    text: string
+}
+
 export interface Question {
     id: string,
     text: string
     difficulty?: number
-    answers_attributes: [
-        {
-            correct: boolean
-            text: string
-        }
-    ]
+    answers_attributes: Answers_Attributes[]
 }
