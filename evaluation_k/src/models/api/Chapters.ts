2d960d8247469591e907282bca93583e53b65dc4
st2diio <st2diio@protonmail.ch>
2021-10-11 20:42:25 +0200

    Updating model Chapters to match requirements

--- evaluation_k/src/models/api/Chapters.ts
+++ evaluation_k/src/models/api/Chapters.ts
@@ -1,25 +1,25 @@
-import { Question, Answers_Attributes } from './Question';
+import { Question } from "./Question";
 
 export interface Chapter {
-        id: number
-        field_id: number
-        level_id: number
-        chapter_id: number
-        name: string
-        number?: number
-        created_at: Date
-        updated_at: Date
-        questions?: Question[]
-        chapters?: [
-                {
-                        id: number
-                        field_id: number
-                        level_id: number
-                        chapter_id: number
-                        name: string
-                        number?: number
-                        created_at: Date
-                        updated_at: Date
-                }
-        ]
+  id: number;
+  field_id: number;
+  level_id: number;
+  chapter_id: number;
+  name: string;
+  number?: number;
+  created_at: Date;
+  updated_at: Date;
+  questions?: Question[];
+  chapters?: [
+    {
+      id: number;
+      field_id: number;
+      level_id: number;
+      chapter_id: number;
+      name: string;
+      number?: number;
+      created_at: Date;
+      updated_at: Date;
+    }
+  ];
 }
