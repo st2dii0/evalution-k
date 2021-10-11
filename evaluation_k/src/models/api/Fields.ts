72805e529907cc3d570c8ade6c160658790ea082
st2diio <st2diio@protonmail.ch>
2021-10-11 20:43:25 +0200

    Adding model Field & Level

---
+++ evaluation_k/src/models/api/Fields.ts
@@ -0,0 +1,6 @@
+export interface Fields {
+    id: number,
+    name: string,
+    created_at: Date,
+    updated_at: Date
+}
---
+++ evaluation_k/src/models/api/Levels.ts
@@ -0,0 +1,6 @@
+export interface Levels {
+    id: number,
+    name: string,
+    created_at: Date,
+    updated_at: Date
+}
