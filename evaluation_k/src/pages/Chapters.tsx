76481c17a317f8f487650a1e3b98bee6daee9131
st2diio <st2diio@protonmail.ch>
2021-10-11 19:30:58 +0200

    refacto & adding logo

--- evaluation_k/src/pages/Chapters.tsx
+++ evaluation_k/src/pages/Chapters.tsx
@@ -1,122 +1,141 @@
-import React, { useState, CSSProperties, useEffect } from 'react'
-import { UserGlobalState } from '../core/user'
-import { useHistory, Link, useParams } from "react-router-dom"
+import React, { useState, CSSProperties, useEffect } from "react";
+import { UserGlobalState } from "../core/user";
+import { useHistory, Link, useParams } from "react-router-dom";
 
-import axios from 'axios'
-import { Container, Content, Button, Icon, IconButton } from 'rsuite'
+import axios from "axios";
+import { Container, Content, Button, Icon, IconButton, Header } from "rsuite";
 
-import { Chapter } from '../models/api/Chapters'
+import { Chapter } from "../models/api/Chapters";
 
-require('dotenv').config()
+require("dotenv").config();
 
-const defaultChapter: Chapter[] = []
+const defaultChapter: Chapter[] = [];
 
 const chapterStyles: CSSProperties = {
-    marginLeft: 30,
-    marginTop: 60,
-    marginBottom: 50,
-    textDecoration: 'none'
-}
+  marginLeft: 20,
+  marginBottom: 50,
+  textDecoration: "none"
+};
 
 const chapterDisplayStyles: CSSProperties = {
-    display: 'flex',
-}
+  display: "flex"
+};
 
 const deleteButtonStyles: CSSProperties = {
-    display:' flex', 
-    justifyContent: 'flex-end',
-    backgroundColor: 'red',
-    color: '#ffffff',
-    marginLeft: 60,
-}
+  display: " flex",
+  justifyContent: "flex-end",
+  backgroundColor: "red",
+  color: "#ffffff",
+  marginLeft: 60
+};
 
 const headerStyles: CSSProperties = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignContent: 'center',
-    columnGap: 200,
-    marginBottom: 20,
-}
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  alignContent: "center",
+  columnGap: 200,
+  marginBottom: 20
+};
 
 const buttonStyles: CSSProperties = {
-    backgroundColor: '#0D67F2',
-    color: '#ffffff',
-}
+  backgroundColor: "#0D67F2",
+  color: "#ffffff"
+};
 
 export const Chapters = () => {
-    const [{ user }] = UserGlobalState()
-    const history = useHistory()
-    const host = process.env.REACT_APP_BASEURL
-    const [ chapters, setChapters ]: [Chapter[], (chapters: Chapter[]) => void ] = useState(defaultChapter)
-    const [loading, setLoading]: [boolean, (loading: boolean) => void] = useState<boolean>(false)
-    const [error, setError]: [string, (error: string) => void] = useState("")
+  const [{ user }] = UserGlobalState();
+  const history = useHistory();
+  const host = process.env.REACT_APP_BASEURL;
+  const [chapters, setChapters]: [
+    Chapter[],
+    (chapters: Chapter[]) => void
+  ] = useState(defaultChapter);
+  const [loading, setLoading]: [boolean, (loading: boolean) => void] = useState<
+    boolean
+  >(true);
+  const [error, setError]: [string, (error: string) => void] = useState("");
 
-    const handleClickNewchapter = () => {
-        history.push({
-            // {/*TODO: Change this url when new features comes */}
-            pathname: `/matiere/${1}/niveau/${1}/chapitre/nouveau`
-        })
-    }
+  const handleClickNewchapter = () => {
+    history.push({
+      // {/*TODO: Change this url when new features comes */}
+      pathname: `/matiere/${1}/niveau/${1}/chapitre/nouveau`
+    });
+  };
 
-    useEffect( () => {
-        axios.get<Chapter[]>(`${host}/v1/chapters/`, {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(response => {
-                setChapters(response.data)
-                setLoading(true)
-            })
-            .catch(err => {
-                setError(err)
-                setLoading(true)
-            })
-    }, [])
+  useEffect(() => {
+    axios
+      .get<Chapter[]>(`${host}/v1/chapters/`, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {
+        setChapters(response.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
 
-    const handleDeleteChapter = (id: number) => {    
-        axios.delete(`${host}/v1/chapters/${id}`)
-            .then( response => {
-                if(response.status === 204){
-                    setChapters(chapters.filter(chapter => chapter.id !== id))
-                }
-            }
-        )
-    }
+  const handleDeleteChapter = (id: number) => {
+    axios.delete(`${host}/v1/chapters/${id}`).then(response => {
+      if (response.status === 204) {
+        setChapters(chapters.filter(chapter => chapter.id !== id));
+      }
+    });
+  };
 
-    return (
-        <Container
-            style={chapterStyles}
-        >
-            <Content
-                style={headerStyles}
-            >
-                <h1> Chapitre: Mathématiques 3ème </h1>
-                <Button 
-                    onClick={()=> handleClickNewchapter()}
-                    style={buttonStyles}
-                > + Ajouter un chapitre </Button>
-            </Content>
-            <Content
-                style={chapterStyles}
-            >
-                {chapters.map((chapter, idx) => (
-                    <div key={idx}>
-                        <div style={chapterDisplayStyles}> 
-                        <Link to={`/chapitres/${chapter.id}`}> 
-                            { chapter.number != null ? `${chapter.number}. ` :  ''} {chapter.name}  
-                        </Link> 
-                        <IconButton 
-                            style={deleteButtonStyles} 
-                            onClick={()=>handleDeleteChapter(chapter.id)} 
-                            icon={<Icon icon="trash2" />} 
-                        />
-                        </div> 
-                        <br/>
-                    </div>
-                ))}
-            </Content>
-        </Container>
-    )
-}
+  return (
+    <Container 
+        style={chapterStyles}
+    >
+        <Header>
+        <img
+          src={require("../assets/mymentor.jpeg").default}
+          alt="mymentor"
+          style={{
+            maxWidth: 200,
+            width: 150,
+            marginLeft: 10
+          }}
+        />
+      </Header>
+      <Content style={headerStyles}>
+        <h1> Chapitre: Mathématiques 3ème </h1>
+        <Button onClick={() => handleClickNewchapter()} style={buttonStyles}>
+          {" "}
+          + Ajouter un chapitre{" "}
+        </Button>
+      </Content>
+      <Content style={chapterStyles}>
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h2>Error fetching data</h2>
+        ) : (
+          <>
+            {chapters.map((chapter, idx) => (
+              <div key={idx}>
+                <div style={chapterDisplayStyles}>
+                  <Link to={`/chapitres/${chapter.id}`}>
+                    {chapter.number != null ? `${chapter.number}. ` : ""}{" "}
+                    {chapter.name}
+                  </Link>
+                  <IconButton
+                    style={deleteButtonStyles}
+                    onClick={() => handleDeleteChapter(chapter.id)}
+                    icon={<Icon icon="trash2" />}
+                  />
+                </div>
+                <br />
+              </div>
+            ))}
+          </>
+        )}
+      </Content>
+    </Container>
+  );
+};
