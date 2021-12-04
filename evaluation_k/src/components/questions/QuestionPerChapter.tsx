df6c6739a2a4a8c2e48cf73bd1ad69545874baa7
st2diio <st2diio@protonmail.ch>
2021-12-04 22:57:53 +0100

    Updating Component QuestionPerChapter

--- evaluation_k/src/components/questions/QuestionPerChapter.tsx
+++ evaluation_k/src/components/questions/QuestionPerChapter.tsx
@@ -1,29 +1,46 @@
 import React, { CSSProperties, useState } from "react";
-import { Radio, Button } from "rsuite";
+import { Button, RadioGroup, Radio } from "rsuite";
 
 import { Chapter } from '../../models/api/Chapters';
-import { Question } from "../../models/api/Question";
+import { Question, Answers_Attributes } from "../../models/api/Question";
 import usePagination from "./Pagination";
+import { localChoices } from '../../pages/Evaluation'
 
 type ChapterProps = {
     chapter: Chapter
+    saveAnswer: (question_id: number, answer_id: number) => void
     previousChapter: () => void
     nextChapter: () => void
+    localChoices: localChoices[]
 }
 
-export const QuestionPerChapter: React.FunctionComponent<ChapterProps> = ({chapter, nextChapter, previousChapter}) => {
-  const {
-    firstContentIndex,
-    lastContentIndex,
-    nextPage,
-    prevPage,
-    page,
-    setPage,
-    totalPages
-  } = usePagination({
-    contentPerPage: 1,
-    count: chapter.questions.length
-  });
+export const QuestionPerChapter: React.FunctionComponent<ChapterProps> = ({chapter, nextChapter, previousChapter, saveAnswer, localChoices}) => {
+
+    const displayChoiceRadio = (question_id: number, answer_id: number): boolean => {
+      // console.log('answer_id: ', answer_id)
+      let index = localChoices.findIndex(obj => obj.question_id === question_id)
+      if (index === -1 ) {        
+        return false
+      } else {
+        if (localChoices[index].answer_id === answer_id) {
+          return true
+        } 
+      }
+      return false
+    }
+
+    const {
+      firstContentIndex,
+      lastContentIndex,
+      nextPage,
+      prevPage,
+      page,
+      setPage,
+      totalPages
+    } = usePagination({
+      contentPerPage: 1,
+      count: chapter.questions.length
+    });
 
   return (
     <div>
@@ -40,6 +57,7 @@ export const QuestionPerChapter: React.FunctionComponent<ChapterProps> = ({chapt
             onClick={() => setPage(el + 1)}
             key={el}
             className={`pageEval ${page === el + 1 ? "active" : ""}`}
+            // {... console.log('Active page',page)}
           >
             {el + 1}
           </button>
@@ -57,20 +75,28 @@ export const QuestionPerChapter: React.FunctionComponent<ChapterProps> = ({chapt
             .slice(firstContentIndex, lastContentIndex)
             .map((question: Question, idx) => (
               <div key={idx}>
-                {/* Display Questions 1 by 1   */}
+                {/* Display Questions 1 by 1 */}
                 <h4>{question.text}</h4>
-                {console.log("Question: ", question)}
-                {question !== undefined ? (
-                  question.answers.map((anwser, index) => (
-                    <div key={index}>
-                      <Radio> {anwser.text} </Radio>
-                    </div>
-                  ))
-                ) : (
-                  <> </>
-                )}
+                {/* {console.log("Question: ", question)} */}
+                  {question !== undefined ? (
+                    question.answers.map((answer) => (
+                      <Radio
+                        key={answer.id}
+                        value={`value-${answer.id}`}
+                        onChange={() => saveAnswer(question.id , answer.id)}
+                        checked={displayChoiceRadio(question.id , answer.id)? (
+                          true
+                          ) : false }
+                      > {answer.text} </Radio>
+                    ))
+                    ) : (
+                      <> </>
+                      )}
                 <div>
-                  <Button onClick={() => {}}>Enregister</Button>
+                  <Button 
+                    onClick={page === totalPages ? nextChapter : nextPage}
+                    className={`pageEval ${page === totalPages && "disabled"}`}
+                  >Enregister</Button>
                 </div>
               </div>
             ))
