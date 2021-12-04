a7dac22394ef00b4ce376134b1ba58c9d578f208
st2diio <st2diio@protonmail.ch>
2021-12-04 23:01:33 +0100

    Updating model Evaluation & packaje.json

--- evaluation_k/src/models/api/Evaluation.ts
+++ evaluation_k/src/models/api/Evaluation.ts
@@ -1,8 +1,8 @@
 export interface Evaluation {
     student_name: string,
     school_uuid?: string // "Snw_mS1bnirK2taMhW-2qw", not working yet
-    school_id: number,
-    user_id: number,
+    school_id?: number,
+    user_id?: number,
     choices_attributes: [
         {
             answer_id: number
